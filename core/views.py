8640997a879f44418415a5aea7c4e1ae7b5ebaa2
YawOtuo <yotuo2003@gmail.com>
2024-06-05 22:05:55 +0000

    fixing migrations issue

--- core/views.py
+++ core/views.py
@@ -1,11 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def test(request):
-    return HttpResponse("Hello, world. You're at the bookings index.")
