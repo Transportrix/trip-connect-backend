42c7dcbf60d5ac22a7e372c272226ebd91187e75
YawOtuo <yotuo2003@gmail.com>
2024-07-08 15:51:35 +0000

    Remove `list_display` attribute from `UserAdmin` class.

--- core/admin.py
+++ core/admin.py
@@ -10,7 +10,6 @@ from core.models.users import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'created_at', 'updated_at')
     search_fields = ('username', 'email')
     readonly_fields = ('created_at', 'updated_at')
 
