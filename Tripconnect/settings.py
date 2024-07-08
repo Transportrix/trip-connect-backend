5cfe653d0f5d6d276b48d03ab2970860bce232af
YawOtuo <yotuo2003@gmail.com>
2024-07-08 19:29:00 +0000

    "Update CORS_ALLOWED_ORIGINS in settings.py"

--- Tripconnect/settings.py
+++ Tripconnect/settings.py
@@ -104,7 +104,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3002",
-    "https://foreman-staging.vercel.app",
+    "https://trip-connect-flax.vercel.app",
 ]
 
 
