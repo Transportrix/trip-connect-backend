b06d47233542e6e16686a6c62ffc017424c21f8e
YawOtuo <yotuo2003@gmail.com>
2024-07-08 15:29:46 +0000

    "Added CORS headers and rest framework to settings.py"

--- Tripconnect/settings.py
+++ Tripconnect/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core",
     "drf_yasg",
+    "corsheaders",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -56,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "Tripconnect.urls"
@@ -96,12 +99,22 @@ DATABASES = {
     }
 }
 
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3002",
     "https://foreman-staging.vercel.app",
-    
+]
+
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
 ]
 
 
