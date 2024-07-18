fc70b35789ffd7839e31e1535d8813ba6c670cfb
YawOtuo <yotuo2003@gmail.com>
2024-07-18 19:00:45 +0000

    "Updated TransportSchedules views: changed string quotes, modified query filtering, and refactored travelling_from_to response"

--- core/views/transportschedules.py
+++ core/views/transportschedules.py
@@ -11,13 +11,13 @@ from core.serializers.transportschedules import TransportSchedulesSerializer
 
 class TransportSchedulesList(APIView):
     def get(self, request):
-        travelling_from = request.query_params.get('travelling_from', None)
-        travelling_to = request.query_params.get('travelling_to', None)
+        travelling_from = request.query_params.get("travelling_from", None)
+        travelling_to = request.query_params.get("travelling_to", None)
 
         if travelling_from and travelling_to:
             schedules = TransportSchedules.objects.filter(
                 travelling_from__icontains=travelling_from,
-                travelling_to__icontains=travelling_to
+                travelling_to__icontains=travelling_to,
             )
             print(schedules.query)  # Print the SQL query being executed
 
@@ -43,9 +43,20 @@ class TransportSchedulesDetail(APIView):
 
 class TransportTravellingFromToList(APIView):
     def get(self, request):
-        travelling_from_to = TransportSchedules.objects.values(
+        travelling_from_to = TransportSchedules.objects.values_list(
             "travelling_from", "travelling_to"
         ).distinct()
-        return Response(travelling_from_to)
 
+        travelling_from_set = set()
+        travelling_to_set = set()
 
+        for travelling_from, travelling_to in travelling_from_to:
+            travelling_from_set.add(travelling_from)
+            travelling_to_set.add(travelling_to)
+
+        result = [
+            {"travelling_from": list(travelling_from_set)},
+            {"travelling_to": list(travelling_to_set)},
+        ]
+
+        return Response(result)
